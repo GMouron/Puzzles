f832cb860aa08c3ac923b2da26555ec5e5790cfe
Guillaume Mouron <guillaume.mouron@gmail.com>
2012-04-10 19:28:18 +0200

    Added conversion from RPN to infix notation

--- Seven/RPN.hs
+++ Seven/RPN.hs
@@ -5,12 +5,24 @@ for first version
 Will create improved version later
 --}
 module RPN
-( evaluateRPN
+( evaluateRPN, rpnToInfix
 ) where
 
-evaluateRPN ::(Read a, Floating a) => String -> Maybe a
+evaluateRPN :: (Read a, Floating a) => String -> Maybe a
 evaluateRPN expr = let res =  foldl calculate [] $ words expr
 					in if(length res > 1) then Nothing else Just (head res)
+
+rpnToInfix :: String -> Maybe String
+rpnToInfix expr = let res = foldl rpnToInfix' [] $ words expr
+					in if(length res > 1) then Nothing else Just (head res)
+
+
+rpnToInfix' :: [String] -> String -> [String]
+rpnToInfix' xs op
+	| length xs >= 2 = let subxs = init xs
+						in (init subxs) ++ readOp (last subxs) (last xs) op
+	| otherwise = xs ++ [op]
+
 
 calculate :: (Read a, Floating a) => [a] -> String -> [a]
 calculate xs op
@@ -26,4 +38,11 @@ applyOp x y op
     | op == "/" = [x / y]
     | otherwise = [x,y,read op]
 
+
+readOp :: String -> String -> String -> [String]
+readOp x y op
+    | op == "+" || op == "-" = ["(" ++ expr ++ ")"]
+    | op == "*" || op == "/"  = [expr]
+    | otherwise = [x,y,op]
+	where expr = x ++ " " ++ op ++ " " ++ y
 
